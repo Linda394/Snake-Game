5e822df083f480a1a1056a0dbb9b66bb4ae42e0c
moses Linda <moseslinda@outlook.com>
2017-02-14 09:40:32 +0200

    Frame development

---
+++ Project1/Frame.h
@@ -0,0 +1,24 @@
+#pragma once
+#include<iostream>
+#include<string.h>
+
+using namespace std;
+#ifndef Frame_h
+#define Frame_h
+
+class Frame {
+public:
+	//Default Constructor and Destructor
+	Frame();
+	~Frame();
+	//Overload Constructor
+
+private:
+	//Member variables
+
+
+};
+
+
+#endif
+
---
+++ Project1/Main.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<string.h>
+#include"Frame.h"
+
+
+using namespace std;
+
+int main()
+{
+
+
+
+
+	return 0;
+}
